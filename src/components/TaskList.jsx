d9e9ec88f26e8304c0db653587c6d0bfa539c104
jignesh3500 <gohelji2004@gmail.com>
2026-02-05 13:04:07 +0530

    [g][added two files]

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,44 +1,49 @@
-import React from "react";
+import { useEffect } from "react";
 
-const TaskList = () => {
+const TaskList = ({ tasks }) => {
+useEffect(()=>{
+  console.log(tasks);
+},[]);
   return (
     <>
       <div className="task-grid">
-        <div className="task-card" style={{ position: "relative" }}>
-          <h3>Complete React Assignment</h3>
-          <p>Finish task manager UI and styling</p>
+        {tasks.map((task) => (
+          <div className="task-card" style={{ position: "relative" }}>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
 
-          <div className="task-meta">
-            <span>Due: 2026-02-10</span>
-            <span className="priority-badge priority-high">High</span>
+            <div className="task-meta">
+              <span>Due: {task.dueDate}</span>
+              <span className="priority-badge priority-high">{task.priority}</span>
+            </div>
+
+            <div className="task-actions">
+              <button
+                className="btn-icon"
+                style={{ background: "#00d2ff" }}
+                title="Edit Task"
+              >
+                ✏️
+              </button>
+
+              <button
+                className="btn-icon"
+                style={{ background: "#00b894" }}
+                title="Mark Complete"
+              >
+                ✔️
+              </button>
+
+              <button
+                className="btn-icon"
+                style={{ background: "#ff416c" }}
+                title="Delete Task"
+              >
+                🗑️
+              </button>
+            </div>
           </div>
-
-          <div className="task-actions">
-            <button
-              className="btn-icon"
-              style={{ background: "#00d2ff" }}
-              title="Edit Task"
-            >
-              ✏️
-            </button>
-
-            <button
-              className="btn-icon"
-              style={{ background: "#00b894" }}
-              title="Mark Complete"
-            >
-              ✔️
-            </button>
-
-            <button
-              className="btn-icon"
-              style={{ background: "#ff416c" }}
-              title="Delete Task"
-            >
-              🗑️
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,59 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom'
-import TaskList from '../components/TaskList'
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import TaskList from "../components/TaskList";
+import TaskForm from "../components/TaskForm";
 
-const Dashboard =() => {
-const navigate = useNavigate()
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
 
-  const handleLogout=()=>{
-        console.log('click from dashboard')
-        localStorage.removeItem('loginData')
-        localStorage.removeItem('authData')
-        // localStorage.clear()
-        navigate('/login')
-  }
-  
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    localStorage.removeItem("authData");
+    // localStorage.clear()
+    navigate("/login");
+  };
+
+  const handleAddTask = async(newTask) => {
+    const tasktoAdd = {...newTask, completed: false}
+    try {
+      const response = await fetch("http://localhost:3000/tasks",{
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(tasktoAdd)
+    });
+    console.log(tasktoAdd);
+    const data = await response.json();
+    setTasks([...tasks, data])
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <Navbar title="Task Management" onLogout={handleLogout}/>
-        <h1>
-            hello guys ....!
-        </h1>
-        <TaskList/>
+      <Navbar title="Task Management" onLogout={handleLogout} />
+      <TaskForm addTask={handleAddTask} />
+      <h1>MY TASKS</h1>
+      <TaskList tasks={tasks} />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
